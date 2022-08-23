63d7c6bcce8cea23e0a8bedead589e09d96e8a64
devil1329 <68421295+devil1329@users.noreply.github.com>
2022-08-23 16:35:53 +0530

    add Controllers

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,10 +1,90 @@
+const model = require('../models/model.js');
 
+// post: http://localhost:8080/api/categories
+async function create_Categories(req,res){
+    const Create = new model.Categories({
+        type: "Investment",
+        color: "#fcbe44"
+    })
+    
+    await Create.save(function(err){
+        if(!err) return res.json(Create);
+        
+        return res.status(400).json({message : `Error while creating categories ${err}`})
+    })
+    
+}
 
-// get categories
-function get_Categories(req,res){
-    res.json("Get Request from Categories");
+// get: http://localhost:8080/api/categories
+async function get_Categories(req,res){
+    let data = await model.Categories.find({})
+    let filter = await data.map(v=> Object.assign({}, {type:v.type, color:v.color}));
+    return res.json(filter);
+}
+
+// post: http://localhost:8080/api/transactions
+async function create_Transaction(req,res){
+    if(!req.body) return res.status(400).json("Post HTTP data not Provided");
+    let {name, type, amount} = req.body;
+
+    const Create = await new model.Transaction(
+        {
+            name,
+            type,
+            amount,
+            date : new Date() 
+        }
+    )
+    
+    await Create.save(function(err){
+        if(!err) return res.json(Create);
+        
+        return res.status(400).json({message : `Error while creating transaction ${err}`})
+    })
+}
+
+// get: http://localhost:8080/api/transactions
+async function get_Transaction(req,res){
+    let data = await model.Transaction.find({})
+    let filter = await data.map(v=> Object.assign({}, {name:v.name, type:v.type, amount:v.amount}));
+    return res.json(filter);
+}
+
+// delete: http://localhost:8080/api/transactions
+async function delete_Transaction(req,res){
+    if(!req.body) res.status(400).json({message : `request body not Found`});
+    await model.Transaction.deleteOne(req.body,function(err){
+        if(!err) return res.json("Record Deleted");
+    }).clone().catch(function(err){res.json("Error while deleting Transaction Record")})
+}
+
+// get: http://localhost:8080/api/labels
+async function get_labels(req,res){
+    model.Transaction.aggregate([
+        {
+            $lookup : {
+                from : "categories",
+                localField: 'type',
+                foreignField:"type",
+                as:"categories_info"
+            }
+        },
+        {
+            $unwind:"$categories_info"
+        }
+    ]).then(result => {
+        let filter =  result.map(v=> Object.assign({}, {_id:v._id, name:v.name, type:v.type, amount:v.amount, color: v.categories_info['color']}));
+        return res.json(filter);
+    }).catch(error => {
+        res.status(400).json("Lookup Collection Error")
+    })
 }
 
 module.exports = {
-    get_Categories
+    create_Categories,
+    get_Categories,
+    create_Transaction,
+    get_Transaction,
+    delete_Transaction,
+    get_labels
 }
--- server/routes/route.js
+++ server/routes/route.js
@@ -2,6 +2,15 @@ const routes = require('express').Router();
 const controller = require('../controller/controller');
 
 routes.route('/api/categories')
-    .get(controller.get_Categories);
+    .post(controller.create_Categories)
+    .get(controller.get_Categories)
 
+routes.route('/api/transactions')
+    .post(controller.create_Transaction)
+    .get(controller.get_Transaction)
+    .delete(controller.delete_Transaction)
+
+routes.route('/api/labels')
+    .get(controller.get_labels)
+    
 module.exports = routes;
