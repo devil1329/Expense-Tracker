4b6fb343661bc3432af0435100b6fa02cc171830
devil1329 <68421295+devil1329@users.noreply.github.com>
2022-08-23 14:20:01 +0530

    connecting mongodb

--- server/server.js
+++ server/server.js
@@ -12,9 +12,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// using routes
-app.use(require('./routes/route'))
+// mongodb connection
+const conn = require("./db/connection.js");
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+// using routes
+app.use(require('./routes/route'));
+
+conn.then(db=>{
+    if(!db) return process.exit(1);
+
+    // listen to http server 
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    })
+
+    app.on('error',err => console.log(`Failed to connect to HTTP server : ${err}`))
+
+    // error in mongodb conn 
+    
+}).catch(error => {
+    console.log(`Connection Failed...! ${error}`);
 })
